8a20cd04a8cf3fe12fc4bb40d8fa7174e946a3ef
Afonso Remédios <afonso.remedios@isel.pt>
2024-09-13 22:56:35 +0100

    Revisions

---
+++ Revisions/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+    sourceSets.all {
+        languageSettings {
+            optIn("kotlin.ExperimentalStdlibApi")
+        }
+    }
+}
