2bb37b58b004be8b96b91dea9d51c53bf4be0f00
Afonso Remédios <afonso.remedios@isel.pt>
2024-09-20 22:39:37 +0100

    -AddDays, equality and toString

--- Date/src/main/kotlin/isel/tds/Date.kt
+++ Date/src/main/kotlin/isel/tds/Date.kt
@@ -26,14 +26,23 @@ package org.example.isel.tds
 //}
 
 
-class Date(val year:Int, val month: Int = 1, val day: Int = 1) {
+class Date(val year:Int, val month: Int = 1, val day: Int = 1):Any() {
+    private val daysOfMonth = listOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
     init {
+        require(year in 1500..4000)
         require(month in 1..12)
-        //TODO validate year and day
+        require(day in 1..lastDayOfMonth)
     }
 
-    private val daysOfMonth = listOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    override operator fun equals(other: Any?) =
+        other is Date
+                && year == other.year && month == other.month && day == other.day
+
+    override fun hashCode() = (year * 12 + month) * 31 + day
+
+    override fun toString() = "$year-%02d-%02d".format(month, day)
+
     val lastDayOfMonth: Int
         get() = if (month==2 && isLeapYear()) 29 else daysOfMonth.get(month-1)
 }
@@ -41,5 +50,25 @@ class Date(val year:Int, val month: Int = 1, val day: Int = 1) {
 fun Date.isLeapYear() = year.isLeapYear()
 fun Int.isLeapYear() = this%4==0 && this%100!=0 || this%400==0
 
+operator fun Date.plus(daysToAdd: Int): Date = this.addDays(daysToAdd)
+operator fun Int.plus( date: Date): Date = date.addDays(this)
 
+private fun Date.addDays(daysToAdd: Int): Date = when{
+    day + daysToAdd <= lastDayOfMonth ->Date(year, month, day + daysToAdd) // x() //Force an StackOverflowError
+    month < 12 -> Date(year, month + 1, 1).addDays(daysToAdd - (lastDayOfMonth - day + 1))
+    else -> Date(year + 1, 1, 1).addDays(daysToAdd - (lastDayOfMonth - day + 1))
+}
 
+tailrec private fun Date.addDaysTailRec(daysToAdd: Int): Date = when{
+    day + daysToAdd <= lastDayOfMonth ->Date(year, month, day + daysToAdd) // x() //Force an StackOverflowError
+    month < 12 -> Date(year, month + 1, 1).addDaysTailRec(daysToAdd - (lastDayOfMonth - day + 1))
+    else -> Date(year + 1, 1, 1).addDaysTailRec(daysToAdd - (lastDayOfMonth - day + 1))
+}
+
+//fun x():Date = x() //Force an StackOverflowError
+
+operator fun Date.compareTo(date: Date): Int = when {
+    year != date.year -> year - date.year
+    month != date.month -> month - date.month
+    else -> day - date.day
+}
--- Date/src/test/kotlin/isel/tds/DateTest.kt
+++ Date/src/test/kotlin/isel/tds/DateTest.kt
@@ -1,7 +1,9 @@
 package isel.tds
 
 import org.example.isel.tds.Date
+import org.example.isel.tds.compareTo
 import org.example.isel.tds.isLeapYear
+import org.example.isel.tds.plus
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFailsWith
@@ -70,8 +72,105 @@ class DateTest {
     @Test
     fun `test Invalid State`() {
 
+        assertFailsWith<IllegalArgumentException> { Date(1000)  }
         assertFailsWith<IllegalArgumentException> { Date(2024,0)  }
-
+        assertFailsWith<IllegalArgumentException> { Date(2024,13)  }
+        assertFailsWith<IllegalArgumentException> { Date(2024, 9, 31)  }
+        assertFailsWith<IllegalArgumentException> { Date(2024, 2, 30)  }
+        assertFailsWith<IllegalArgumentException> { Date(2023, 2, 29)  }
 
     }
+
+    @Test
+    fun testAddDaysToDateTest(){
+        val date = Date(2024, 9, 20) + 5
+
+        assertEquals(2024, date.year, "Year not valid")
+        assertEquals(9, date.month)
+        assertEquals(25, date.day)
+    }
+
+    @Test
+    fun testAddDateToDaysTest(){
+        val date = 5 + Date(2024, 9, 20)
+
+        assertEquals(2024, date.year, "Year not valid")
+        assertEquals(9, date.month)
+        assertEquals(25, date.day)
+    }
+
+    @Test
+    fun testAddDaysToDateWithMonthIncrementTest(){
+        val date = Date(2024, 9, 20) + 15
+
+        assertEquals(2024, date.year, "Year not valid")
+        assertEquals(10, date.month)
+        assertEquals(5, date.day)
+    }
+
+    @Test
+    fun testAddDaysToDateWith2MonthIncrementTest(){
+        val date = Date(2024, 9, 20) + 45
+
+        assertEquals(2024, date.year, "Year not valid")
+        assertEquals(11, date.month)
+        assertEquals(4, date.day)
+    }
+
+    @Test
+    fun testAddDaysToDateWithYearIncrementTest(){
+        val date = Date(2024, 9, 20) + 107
+
+        assertEquals(2025, date.year, "Year not valid")
+        assertEquals(1, date.month)
+        assertEquals(5, date.day)
+    }
+
+
+    @Test fun `Test equality of dates that represent the same date`() {
+        val sut = Date(2023, 3, 2)
+        assertEquals(sut, sut)             //   => assertTrue(sut == sut)
+        assertEquals(sut, Date(2023, 3, 2))
+    }
+
+
+    @Test fun `Tests inequality between dates and dates with another type`() {
+        val sut = Date(2023, 3, 2)
+        assertNotEquals(sut, Date(2023, 4, 1))
+        val any :Int = 2023
+        assertNotEquals(sut, any)  // Compare with Int
+        val dn :Date? = null
+        assertNotEquals(sut, dn)   // Compare with null
+        assertNotEquals(dn, sut)   // Compare null with Date
+    }
+
+
+    @Test fun `Coherence between equals and hashCode`() {
+        val sut = Date(2023, 3, 2)
+        assertEquals(sut.hashCode(), sut.hashCode())
+        val d2 = Date(2023, 3, 2)
+        assertEquals(sut, d2)
+        assertEquals(sut.hashCode(), d2.hashCode()) // Must be the same
+        val d3 = Date(2023, 4, 2)
+        assertNotEquals(sut, d3)
+        assertNotEquals(sut.hashCode(), d3.hashCode()) // Should be different
+    }
+
+
+    @Test fun `Compare dates`() {
+        val sut = Date(2023, 3, 2)
+        assertTrue(sut < Date(2023, 3, 4))
+        assertTrue(Date(2023, 4, 2) >= sut)
+        assertTrue(sut <= Date(2024, 3, 2))
+        assertTrue(sut > Date(2023, 3, 1))
+    }
+
+    @Test fun `String representation of a date`() {
+        val sut = Date(2023, 3, 2)
+        assertEquals("2023-03-02", sut.toString())
+    }
 }
+
+
+
+
