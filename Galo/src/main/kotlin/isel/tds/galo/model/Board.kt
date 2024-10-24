53a4b03df3f764a0f90914802bdc91adcd050d38
Afonso Remédios <afonso.remedios@isel.pt>
2024-10-24 21:45:26 +0100

    Tic tac toe improvements (Board States for pattern matching, Map<Position,Player> and winner in board state)

--- Galo/src/main/kotlin/isel/tds/galo/model/Board.kt
+++ Galo/src/main/kotlin/isel/tds/galo/model/Board.kt
@@ -2,43 +2,98 @@ package isel.tds.galo.model
 
 const val BOARD_SIZE = 3
 const val BOARD_CELLS = BOARD_SIZE * BOARD_SIZE
+//
+//class Board(
+//    val cells: List<Player?> =
+//        List(BOARD_CELLS) { null },
+////        listOf(
+////            ' ', ' ', ' ',
+////            ' ', ' ', ' ',
+////            ' ', ' ', ' '
+////        ),
+//    val turn: Player = Player.X, // X or O
+//    val winner: Player? = null
+//)
+//
+//val Board.isDraw: Boolean get() = winner==null && cells.all { it!=null }
 
-class Board(
-    val cells: List<Player?> =
-        List(BOARD_CELLS) { null },
-//        listOf(
-//            ' ', ' ', ' ',
-//            ' ', ' ', ' ',
-//            ' ', ' ', ' '
-//        ),
-    val turn: Player = Player.X // X or O
-)
-
-
-fun Board.play(pos: Position): Board {
-    check(cells[pos.index] == null) { "Position $pos is already used" }
-
-    return Board(
-        cells = cells.mapIndexed { idx, cellContent -> if (idx == pos.index) turn else cellContent },
-        turn = turn.other
-    )
-}
+//fun Board.play(pos: Position): Board {
+//    check(cells[pos.index] == null) { "Position $pos is already used" }
+//
+//    return Board(
+//        cells = cells.mapIndexed { idx, cellContent -> if (idx == pos.index) turn else cellContent },
+//        turn = turn.other
+//    )
+//}
 
 //fun Board.freeCell(pos: Int): Boolean {
 //    return cells[pos] == null
 //}
 
-fun Board.isWinner(player: Player) =
-    (0..2).any { col -> (0..6 step 3).all { cells[col + it] == player } } ||
-        (0..6 step 3).any { row -> (0..2).all { cells[row + it] == player } } ||
-        (0..8 step 4).all { cells[it] == player } ||
-        (2..6 step 2).all { cells[it] == player }
+//fun Board.isWinner(player: Player) =
+//    (0..2).any { col -> (0..6 step 3).all { cells[col + it] == player } } ||
+//        (0..6 step 3).any { row -> (0..2).all { cells[row + it] == player } } ||
+//        (0..8 step 4).all { cells[it] == player } ||
+//        (2..6 step 2).all { cells[it] == player }
+//
 
 
+//
+//fun Board.play(pos: Position): Board {
+//    check(winner == null) { "Game is over" }
+//    require(cells[pos.index] == null) { "Position $pos is already used" }
+//    val movesAfter = cells.mapIndexed { idx, c ->
+//        if (idx == pos.index) turn else c
+//    }
+//    return Board( movesAfter, turn.other, winnerIn(pos,movesAfter))
+//}
+//
+//
+//private fun winnerIn(pos: Position, moves: List<Player?>): Player? {
+//    val player = checkNotNull(moves[pos.index])
+//    if ( moves.count { it == player } < BOARD_SIZE ) return null
+//    return player.takeIf {
+//        (0..<BOARD_SIZE).all {
+//            moves[it + pos.row * BOARD_SIZE] == player } ||
+//                (0..<BOARD_CELLS step BOARD_SIZE).all {
+//                    moves[it + pos.col] == player } ||
+//                pos.backSlash && (0..<BOARD_CELLS step BOARD_SIZE+1).all {
+//            moves[it] == player } ||
+//                pos.slash && (BOARD_SIZE-1..<BOARD_CELLS-1 step BOARD_SIZE-1).all{
+//            moves[it] == player }
+//    }
+//}
+typealias Moves = Map<Position, Player>
+
+sealed class Board(val moves: Moves)
+class BoardRun( moves: Moves, val turn: Player) : Board(moves)
+class BoardWin(moves: Moves, val winner: Player): Board(moves)
+class BoardDraw(moves: Moves) : Board(moves)
+//class BoardXyz():Board(emptyMap())
+//Produces Error because the when is not exaustive anymore
+
+fun Board.play(pos: Position): Board = when(this) {
+    is BoardRun -> {
+        require(moves[pos] == null) { "Position $pos used" }
+        val moves = moves + (pos to turn)
+        when {
+            isWinner(pos,moves) -> BoardWin(moves, turn)
+            moves.size == BOARD_CELLS -> BoardDraw(moves)
+            else -> BoardRun(moves, turn.other)
+        }
+    }
+    is BoardWin, is BoardDraw -> error("Game over")
+}
 
 
+private fun isWinner(pos: Position, moves: Moves): Boolean =
+    moves.size >= BOARD_SIZE*Player.entries.size-1 &&
+            moves.filter { it.value == moves[pos] }.keys.run {
+                count{ it.row == pos.row } == BOARD_SIZE ||
+                        count{ it.col == pos.col } == BOARD_SIZE ||
+                        count{ it.slash } == BOARD_SIZE ||
+                        count{ it.backSlash } == BOARD_SIZE
+            }
 
 
-
-
-
+fun Board(start: Player = Player.X): Board = BoardRun(emptyMap(),start)
--- Galo/src/main/kotlin/isel/tds/galo/model/Position.kt
+++ Galo/src/main/kotlin/isel/tds/galo/model/Position.kt
@@ -2,6 +2,11 @@ package isel.tds.galo.model
 
 @JvmInline
 value class Position private constructor( val index:Int){
+    val row get() = index / BOARD_SIZE
+    val col get() = index % BOARD_SIZE
+
+    val backSlash get() = row == col
+    val slash get() = row + col == BOARD_SIZE - 1
 
     companion object{
         val values = List(BOARD_CELLS){Position(it)}
